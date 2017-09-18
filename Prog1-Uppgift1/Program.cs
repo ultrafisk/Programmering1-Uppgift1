e760b3c64d802c6a65575b9ebfdee873ed4616b5
Erik Hacker <ultrafisk@users.noreply.github.com>
2017-09-18 13:10:59 +0200

    Lagt till kommentarer
    Nu är det klart

--- Prog1-Uppgift1/Program.cs
+++ Prog1-Uppgift1/Program.cs
@@ -10,30 +10,30 @@ namespace Prog1_Uppgift1
     {
         static void Main(string[] args)
         {
-            char characterNumeroUno = 'E';
-            char characterNumeroDos = 'H';
-            float floatNameYo = 4.5F;
-            int integerNameIsNice = 5;
-            int[] theRealArrayTheBestArray = new int[10];
-            for (int i = 0; i < 10; i++)
+            char characterNumeroUno = 'E'; //skapar en char variabel med värdet E
+            char characterNumeroDos = 'H'; //typ samma som ovan
+            float floatNameYo = 4.5F; //skapar en float
+            int integerNameIsNice = 5; //skapar en integer
+            int[] theRealArrayTheBestArray = new int[10]; //skapar en int array
+            for (int i = 0; i < 10; i++) //ger varje int i arrayen ett värde
             {
                 theRealArrayTheBestArray[i] = i;
             }
-            foreach(int arrayNumber in theRealArrayTheBestArray)
+            foreach(int arrayNumber in theRealArrayTheBestArray) //skriver ut arrayens värden
             {
                 Console.WriteLine(arrayNumber);
             }
-            if (floatNameYo < 4.0)
+            if (floatNameYo < 4.0) //skriver ut en sak om floatNameYo är mindre än 4
             {
                 Console.WriteLine("This Code Is Shit");
             }
-            if (floatNameYo == 4.5F)
+            if (floatNameYo == 4.5F) //skriver ut ngt om floatNameYo är lika med 4.5
             {
                 Console.WriteLine("This Code Is Almost Shit");
             }
-            Console.WriteLine(characterNumeroUno);
-            Console.WriteLine(characterNumeroDos);
-            Console.ReadLine();
+            Console.WriteLine(characterNumeroUno); //skriver ut characterNumeroUno
+            Console.WriteLine(characterNumeroDos); //skriver ut characterNumeroDos
+            Console.ReadLine(); //läser
         }
     }
 }
